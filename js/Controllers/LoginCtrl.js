c2d0540e88c89d926bf39a7bc99a073268732490
RafehSaeed <rafeh_saeed@hotmail.ca>
2016-04-10 13:51:51 -0400

    login check and routing

--- js/Controllers/LoginCtrl.js
+++ js/Controllers/LoginCtrl.js
@@ -4,32 +4,45 @@
 
 function LoginCtrl($scope,$http) { 
 
-  
+ 
 
          this.formData = {
              password: '',
              email: ''
+
         };
 
 
    $scope.Login = function () {
        
        //Send Post Request
-
-     
+    $scope.login_status = ""
     
      var session = JSON.stringify(this.formData);
-     
-     console.log (session);
+ 
+
 
     $http.post('http://localhost:3000/login',session).
     success(function(data, status, headers, config) {
-        // this callback will be called asynchronously
-        // when the response is available
-        console.log(data);
+        // Check if the user doesnt exist
+        
+
+         var datasize = Object.keys(data);
+        
+         
+         if (datasize.length == 1)
+              {
+              $scope.login_status= data.content;
+              console.log(data.content)
+                 
+              }
+      
+    
+        
       }).
       error(function(data, status, headers, config) {
-        // called asynchronously if an error occurs
+
+               // called asynchronously if an error occurs
         // or server returns response with an error status.
       });
     }
