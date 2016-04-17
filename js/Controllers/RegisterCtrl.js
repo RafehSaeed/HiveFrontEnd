55191eaed62d587bc8db25428ac4726b64083871
RafehSaeed <rafeh_saeed@hotmail.ca>
2016-04-17 11:53:35 -0400

    Register Controller/ and view added/ mainpage updated for api demo

---
+++ js/Controllers/RegisterCtrl.js
@@ -0,0 +1,26 @@
+
+
+
+//Controller required to let users register to Hive
+function RegisterCtrl($scope,$http,$location,$window) { 
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+app.controller('RegisterController', RegisterCtrl);
+
+
+
+ 
+
