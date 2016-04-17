91e4ba999fedcac0f9fc649e80a26f7afe1a26d1
RafehSaeed <rafeh_saeed@hotmail.ca>
2016-04-17 18:04:26 -0400

    Registeration implemented

--- js/Controllers/RegisterCtrl.js
+++ js/Controllers/RegisterCtrl.js
@@ -4,7 +4,10 @@
 //Controller required to let users register to Hive
 function RegisterCtrl($scope,$http,$location,$window) { 
 
-      registered = false;
+     $scope.registered = false;
+     $scope.register_status ="";
+
+      //Form for registering login information
       this.formData = {
              username: '',
              password: '',
@@ -14,13 +17,43 @@ function RegisterCtrl($scope,$http,$location,$window) {
 
 
         };
+
+      //Form for registering profile information
+         this.formData = {
+             firstname: '',
+             lastname: '',
+             buisnessname: '',
+             address:'',
+             postalcode:''
+
+
+        };
     //Function used to Register for the application
    $scope.Register = function () {
 
 
 
-   this.registered = true;
+   
    console.log(this.formData);
+
+   var session = JSON.stringify(this.formData);
+ 
+
+
+    $http.post('http://localhost:3000/register',session).
+    success(function(data, status, headers, config) {
+        // Check if the user doesnt exist
+       
+
+          if(data.content=="User can be Registered")
+            {
+            $scope.registered = true;
+            $scope.register_status = "works";
+            }
+          else
+            {$scope.register_status = data.content;}
+         
+})
 }
 
 }
